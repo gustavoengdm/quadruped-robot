22108e25fc85a5cefd7abe508191f8268e428ab2
Gustavo <gustavoengdm@gmail.com>
2018-03-05 13:05:11 -0300

    update

--- software/quadruped/main.cpp
+++ software/quadruped/main.cpp
@@ -5,21 +5,47 @@
  *      Author: gustavo
  */
 
+/**
+ avrdude -Cavrdude.conf -patmega328p -carduino -P/dev/ttyUSB0 -b57600 -D -Uflash:w:/mnt/wdisk/work/projects/robotics/quadruped-robot/eclipse-workspace/tmp/Release/tmp.hex:i
+ **/
+
 #include <avr/io.h>
-#include "servo-service.hpp"
+#include <util/delay.h>
+
+#include "servo_service.hpp"
+
+ServoService::Servo s1;
+ServoService::Servo s2;
 
 void setup(void) {
+
 	ServoService::setup(20000);
+	DDRB = 0xFF;
+	s1.attach(&PORTB, 5);
+	s1.write_us(15000);
+	s2.attach(&PORTB, 4);
+	s2.write_us(10000);
+
+	sei();
 }
 
+// action
+//ISR (TIMER1_COMPA_vect) {
+//	//_delay_ms(50);
+//	PORTB ^= (1 << 5);
+//}
+
 void loop(void) {
+//	_delay_ms(50);
+//	PORTB ^= (1 << 5);
 }
 
 int main() {
 
 	setup();
 
-	while(1) loop();
+	while (1)
+		loop();
 
 	return 0;
 }
---
+++ software/quadruped/servo_service.hpp
@@ -0,0 +1,163 @@
+/*
+ * servo-service.hpp
+ *
+ *  Created on: Mar 4, 2018
+ *      Author: gustavo
+ */
+
+#ifndef SERVO_SERVICE_HPP_
+#define SERVO_SERVICE_HPP_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include <stdint.h>
+
+#include "com.hpp"
+
+#define __prescaler				8
+#define __clock_ticks_per_us	16
+#define us2ticks( us )			( __clock_ticks_per_us / __prescaler * (us) )
+
+#define MAX_NUMBER_OF_SERVOS 12
+
+// definitions in microseconds
+#define FRAME_PERIOD	20000		// 20ms
+#define MIN_PULSE 		500			// 0.5ms
+#define MAX_PULSE 		2000		// 2ms
+
+// Using Timer1
+namespace ServoService {
+
+	class __servo {
+		public:
+			uint8_t pin;
+			volatile uint8_t * port;
+			volatile uint16_t ticks;
+			uint8_t active;
+
+		public:
+			inline void low(void) {
+				__cbi(*port, pin);
+			}
+
+			inline void high(void) {
+				__sbi(*port, pin);
+			}
+	};
+
+	__servo servos[MAX_NUMBER_OF_SERVOS];
+
+	volatile uint16_t __frame_ticks;
+	static uint8_t __nbr_of_servos = 0;
+	static int8_t __servo_count = -1;
+
+	/**
+	 * Configure Timer1 to ServoService
+	 * @param us frame period in microseconds
+	 */
+	void setup(uint16_t us) {
+
+		// compute the number of ticks for the frame period
+		__frame_ticks = us2ticks(us);
+		__nbr_of_servos = 0;
+		__servo_count = -1;
+
+		// set Normal Mode operation
+		TCCR1A = 0;
+
+		// configure prescaler 8
+		TCCR1B = (1 << CS11);
+
+		// clear OCF1A to prevent any old interrupts
+		TIFR1 |= (1 << OCF1A);
+
+		// enable interrupt of Output Compare Register A
+		TIMSK1 |= (1 << OCIE1A);
+
+		// start timer1
+		TCNT1 = 0;
+	}
+
+	class Servo {
+		public:
+			int8_t __index;
+			uint16_t __min_ticks, __max_ticks;
+
+		public:
+			Servo() {
+				__index = 0;
+				__min_ticks = us2ticks(MIN_PULSE);
+				__max_ticks = us2ticks(MAX_PULSE);
+			}
+
+			void attach(volatile uint8_t * port, uint8_t pin) {
+
+				if (__nbr_of_servos >= MAX_NUMBER_OF_SERVOS) {
+					__index = -1;
+					return;
+				}
+
+				__index = __nbr_of_servos++;
+				servos[__index].active = true;
+				servos[__index].port = port;
+				servos[__index].pin = pin;
+				servos[__index].ticks = __min_ticks;
+
+				// Accessing DDRx to configure pin as OUTPUT
+				*(port - 1) |= (1 << pin);
+
+				// Cleaning pin level
+				*port &= ~(1 << pin);
+
+				__nbr_of_servos++;
+			}
+
+			void write_us(uint16_t us) {
+				servos[__index].ticks = us2ticks(us);
+			}
+	};
+
+	void handle_interrupt(void) {
+		if (__servo_count == -1) {
+			// restart counter
+			TCNT1 = 0;
+		} else {
+			// set current Servo pin to low
+			if (__servo_count < __nbr_of_servos) {
+				servos[__servo_count].low();
+			}
+		}
+
+		__servo_count++;
+
+		if (__servo_count < __nbr_of_servos) {
+
+			// update OCR1A to the next TOP value
+			OCR1A = TCNT1 + servos[__servo_count].ticks;
+
+			// Set servo pin high if it is active
+			if (servos[__servo_count].active) {
+				servos[__servo_count].high();
+			}
+
+		} else {
+
+			OCR1A = 0000;
+
+//			if ((TCNT1 + 4) < __frame_ticks) {
+//				OCR1A = __frame_ticks;
+//			} else {
+//				OCR1A = TCNT1 + 4;
+//			}
+			__servo_count = -1;
+		}
+	}
+}
+
+// action
+ISR (TIMER1_COMPA_vect) {
+	ServoService::handle_interrupt();
+}
+
+#endif /* SERVO_SERVICE_HPP_ */
